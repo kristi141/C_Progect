fc2ef3c2037ae6b0b2a8ef0849654282fa7dcd97
kristinatrikoz1010@gmail.com <kristinatrikoz1010@gmail.com>
2024-12-05 23:48:37 +0700

    add changes

--- Controle tests/demo_practice_1.c
+++ Controle tests/demo_practice_1.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <math.h>  
 
-int sumOfNumber(){ // 13 F1
-  int a;
-	int s = 0;
-	
-	printf("Введите число :\n");
-	scanf("%d", &a);
+int sumOfNumber() { // 13 F1
+    int a;
+    int s = 0;
 
-for (int n = a; n != 0; n = n/10){
-	int k = n % 10;
-	s = s + k;
-} 
-	printf("Сумма цифр: %d ", s);
+    printf("Введите число :\n");
+    scanf("%d", &a);
+
+    for (int n = a; n != 0; n = n / 10) {
+        int k = n % 10;
+        s = s + k;
+    }
+    printf("Сумма цифр: %d\n ", s);
 } //F1
 
-int reverseNumber(){ //15 F2
-      int n, s = 0, k, a;
+int reverseNumber() { //15 F2
+    int n, s = 0, k, a;
 
     printf("Введите число: \n");
     scanf("%d", &a);
@@ -29,8 +29,8 @@ int reverseNumber(){ //15 F2
     printf("Перевернутое число: %d\n", s);
 } //F2
 
-int primeNumbers(){ //24 F3
-  int j = 0, s, m;
+int primeNumbers() { //24 F3
+    int j = 0, s, m;
     int n = 6;
     int A[1001];
     int B[1001];
@@ -66,11 +66,12 @@ int primeNumbers(){ //24 F3
     for (int k = 0; k < j; k++) {
         printf("%d ", B[k]);
     }
-   
+    printf("\n");
+
 } //F3
 
-int rangePrimeNumber(){ // 30 F4
-int i = 0, s, a, v, m, j;
+int rangePrimeNumber() { // 30 F4
+    int i = 0, s, a, v, m, j;
     int A[1001];
 
     printf("Введите границу диапазона: \n");
@@ -85,7 +86,7 @@ int i = 0, s, a, v, m, j;
         for (j = 0; j < i; j++) {
 
             if (a % A[j] == 0) {
-                s = 1;  
+                s = 1;
                 break;
             }
         }
@@ -102,11 +103,12 @@ int i = 0, s, a, v, m, j;
     for (j = 0; j < i; j++) {
         printf("%d ", A[j]);
     }
-  
+    printf("\n");
+
 } //F4
 
-int duplicateNumbers(){ // 37 F5
-  int n = 5;
+int duplicateNumbers() { // 37 F5
+    int n = 5;
     int c[1001];
     int i, b, s = 0;
 
@@ -136,11 +138,11 @@ int duplicateNumbers(){ // 37 F5
     }
     else {
         printf("Все элементы уникальны.\n");
-    } 
+    }
 } //F5
 
-int sumToNegativeNumber(){ // 41 F6
-   int k = 0, s = 0, i = 0;
+int sumToNegativeNumber() { // 41 F6
+    int k = 0, s = 0, i = 0;
     int A[1001];
     int n;
 
@@ -154,7 +156,7 @@ int sumToNegativeNumber(){ // 41 F6
     }
 
     i = 0;
-    
+
     for (; i < n && k == 0; i++) {
         if (A[i] < 0) {
             k = 1;
@@ -164,11 +166,11 @@ int sumToNegativeNumber(){ // 41 F6
         }
     }
 
-    printf("Сумма элементов до первого отрицательного: %d\n", s); 
+    printf("Сумма элементов до первого отрицательного: %d\n", s);
 } //F6
 
-int maxNumber(){ // 52 F7
-  int k = 0;
+int maxNumber() { // 52 F7
+    int k = 0;
     int A[1001];
     int n = 6;
 
@@ -187,48 +189,63 @@ int maxNumber(){ // 52 F7
     printf("Максимальный элемент: %d \n", A[k]);
 } //F7
 
-int main(){
-  int x;
-  
-   printf("Выберете функцию для работы:\n ");
-   printf("1.F1\n 2.F2\n 3.F3\n 4.F4\n 5.F5\n 6.F6\n 7.F7\n");
-   
-   scanf("%d", x);
-   
-   switch (x) {
-	    case 1:
-			sumOfNumber();
-			break;
+int main() {
+    int x;
 
-		case 2:
-			reverseNumber();
-			break;
+    printf("Выберете функцию для работы:\n ");
+    printf("1.F1\n 2.F2\n 3.F3\n 4.F4\n 5.F5\n 6.F6\n 7.F7\n");
 
-		case 3:
-			primeNumbers();
-			break;
+    while (scanf("%d", &x)) {
 
-		case 4:
-			rangePrimeNumber();
-			break;
+        switch (x) {
+        case 1:
+            printf("Выбранная функция считает сумму цифр в числе\n\n");
+            sumOfNumber();
+            break;
 
-		case 5:
-			duplicateNumbers();
-			break;
+        case 2:
+            printf("Выбранная функция выводит число в обратном порядке\n\n");
+            reverseNumber();
+            break;
 
-		case 6:
-			sumToNegativeNumber();
-			break;
+        case 3:
+            printf("Выбранная функция выводит простые числа из заданных\n\n");
+            primeNumbers();
+            break;
 
-		case 7:
-			maxNumber();
-			break;
+        case 4:
+            printf("Выбранная функция выводит простые числа из заданного диапазона\n\n");
+            rangePrimeNumber();
+            break;
 
-		default:
-			printf("There is no such day of the week!\n");
-			break;
-	}
-   
-  
-  return 0;
+        case 5:
+            printf("Выбранная функция ищет количество повторений из заданных чисел\n\n");
+            duplicateNumbers();
+            break;
+
+        case 6:
+            printf("Выбранная функция считает сумму элементов в массиве до первого отрицательного\n\n");
+            sumToNegativeNumber();
+            break;
+
+        case 7:
+            printf("Выбранная функция ищет максимальный элемент\n\n");
+            maxNumber();
+            break;
+
+        case 0:
+            printf("Программа остановлена");
+            break;
+
+        default:
+            printf("Выбрана некорректная опция!\n");
+            break;
+        }
+
+        if (x == 0) {
+            break;
+        }
+    }
+
+    return 0;
 }
